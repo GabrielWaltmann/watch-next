409c79eb7ef5b89045ae2e89ac60eaa69fac4f46
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-01-04 10:24:02 -0300

    Fix: hide header then isn’t on screen top

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -5,18 +5,16 @@ import { useRouter } from "next/router";
 import { MagnifyingGlass } from "phosphor-react";
 import { useEffect } from "react";
 
-
-
 export default function Header() {
     // Hide header on Login Page
     const { asPath } = useRouter()
-    useEffect( () =>{
-        if(asPath === '/Login'){
+    useEffect(() => {
+        if (asPath === '/Login') {
             document.querySelector<any | null>('header').classList.add('hidden')
         }
 
         const pages = ['Descobrir/tv', 'Descobrir/movies', 'Descobrir/animes', 'Home']
-        pages.map(name =>{
+        pages.map(name => {
             const url = `a[href='/${name}']`
             const element = document.querySelector(url)
             const index = asPath.indexOf(name)
@@ -26,12 +24,27 @@ export default function Header() {
             isCurrentURL ? setActive() : removeActive()
         })
     }, [asPath])
-    
+
+    // hide header then isn’t on screen top
+    setInterval(() => {
+        function getOffset(el: any) {
+            const rect = el.getBoundingClientRect();
+            return {
+                left: rect.left + window.scrollX,
+                top: rect.top + window.scrollY
+            };
+        }
+        const top = (getOffset(document.querySelector('ul'))).top
+        const header = document.querySelector('header')
+        
+        if(top <= 40) header?.classList.remove('hidden')
+        else header?.classList.add('hidden')
+         
+    }, 100)
 
     return (
         <header className="w-screen fixed top-0">
             <Navbar
-    
                 rounded={true}
                 className='bg-transparent w-full transition-all duration-300 m-0'
             >
@@ -44,8 +57,8 @@ export default function Header() {
                             height={63}
                         />
                     </Navbar.Brand>
-                    
-                    <Navbar.Toggle className="hover:bg-transparent text-white-primary border-0 focus:ring-opacity-0 "/>
+
+                    <Navbar.Toggle className="hover:bg-transparent text-white-primary border-0 focus:ring-opacity-0 " />
                     <Navbar.Collapse>
                         <Link className="text-white-primary text-sm text-center hover:text-gray-3 transition-all duration-300" href="/Home" >
                             Minha Lista
@@ -62,10 +75,10 @@ export default function Header() {
                     </Navbar.Collapse>
                 </div>
 
-                <MagnifyingGlass 
-                className="text-white-primary"
-                height={'40px'} 
-                width={'40px'}
+                <MagnifyingGlass
+                    className="text-white-primary"
+                    height={'40px'}
+                    width={'40px'}
                 />
             </Navbar>
         </header>

be85367483f43f87e7982731ea77feb4b4069aee
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-01-13 14:39:08 -0300

    Build: add Spinner then is loading the items

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from "flowbite-react";
 import Header from "../../components/Header";
 import Text from "../../components/Text";
 import { getTokenOnLocalStorage } from "../Entrar";
@@ -19,6 +20,10 @@ export default function List(){
             <div className="w-full px-32 py-16 flex flex-col gap-4 max-sm:py-4 max-md:px-4">
                 <Text className="text-white-primary text-lg font-bold pl-4 max-sm:pl-0">Assistir a seguir</Text>
 
+                <div className="w-full justify-center items-center mt-16 flex gap-4">
+                    <Spinner aria-label="Default status example" />
+                    <Text className="text-white-primary" >Carregando sua lista...</Text>
+                </div>
 {/*                 <Item
                     title="Stranger Thigns"
                     url="stranger.jpg"

d2b144bdbc8e072fc5522b4ca340dd28f33e8e88
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-01-20 10:58:31 -0300

    Refactor: Otimize Code

--- src/Pages/[CardId].tsx
+++ src/Pages/[CardId].tsx
@@ -1,89 +1,106 @@
-import { useRouter } from "next/router";
+import axios from "axios";
 import Image from "next/image";
-import { GetStaticPaths, GetStaticProps } from 'next'
-import Header from "../components/Header";
-import { api, instance } from "../api/axios";
 import { useEffect, useState } from "react";
-type Params = {
-	params: {
-		slug: string
-	}
-}
+import Header from "../components/Header";
 
-// set posibles Paths
-export const getStaticPaths: GetStaticPaths = async () => {
-    return { paths: [], fallback: true }
-}
+export function getStaticPaths(){ return { paths: [], fallback: true } }
 
+type Params = { params: { slug: string } }
 export const getStaticProps: any = async ({ params }: Params) => {
     const {CardId}: any = params;
-    let datas: any
-    const url = `https://api.themoviedb.org/3/movie/${CardId}?api_key=37515be8a40c641389533f4f4c0724ee`
-    await api(instance).get(url)
-    .then((res)=>datas = (res.data))
-
-
-        return { 
-            props: { datas }
-        }
+    
+    return { 
+        props: { id: CardId }
+    }  
     
 }
 
-type Datas = {
-    backdrop_path: string
-    poster_path: string
-    title: string
-    overview: string,
-    release_date: string
+type MovieProps = {
+    title: string,
+    PosterPath: string,
+    Release_date: string,
+    Overview: string,
+    Episodes: {} | null
 }
 
-export default function Movie({datas}: {datas :  Datas}) {
-    const [PosterURL, setPosterURL] = useState<null | string>(null)
-    
-    const posterDefault = `https://image.tmdb.org/t/p/w500/9xkGlFRqrN8btTLU0KQvOfn2PHr.jpg`
-
+export default function CardId({id}: any){
+    const [title, setTitle] = useState('Title')
+    const [PosterPath, setPosterPath] = useState('/stranger.jpg')
+    const [Overview, setOverview] = useState('Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero accusantium deleniti adipisci sapiente magnam fugiat, atque repellendus suscipit blanditiis sed minima, ducimus assumenda, molestiae vitae numquam. Iure recusandae reprehenderit perferendis.')
+    const [Release_date, setRelease_date] = useState('2000-00-00')
+    const [Episodes, setEpisodes] = useState(null)
+    const movieurl = `https://api.themoviedb.org/3/movie/${id}?api_key=37515be8a40c641389533f4f4c0724ee`
+    const tvurl = `https://api.themoviedb.org/3/tv/${id}?api_key=37515be8a40c641389533f4f4c0724ee`
 
     useEffect(()=>{
-        if(datas){
-            console.log(datas)
-            setPosterURL(`https://image.tmdb.org/t/p/w500${datas.poster_path}`)
+        if(id !== undefined){
+            axios.get(movieurl)
+            .then((res)=>{ 
+                console.log(res.data)
+                setTitle(res.data.title) 
+                setPosterPath('https://image.tmdb.org/t/p/w500/'+res.data.poster_path)
+                setOverview(res.data.overview)
+                setRelease_date(res.data.release_date)
+            })
+            .catch((err)=>{
+                axios.get(tvurl)
+                .then((res)=>{ 
+                    console.log(res.data)
+                    setTitle(res.data.name) 
+                    setPosterPath('https://image.tmdb.org/t/p/w500/'+res.data.poster_path)
+                    setOverview(res.data.overview)
+                    setRelease_date(res.data.first_air_date)
+                })
+                .catch((err)=>{})
+            })
         }
-    }, [datas])
+    }, [id])
 
+    return ( <>
+        <Header />
+        <Body
+        title={title}
+        Overview={Overview}
+        PosterPath={PosterPath}
+        Release_date={Release_date}
+        Episodes={Episodes}
+        />
+    </> )
+}
+
+function Body({title, PosterPath, Release_date, Overview, Episodes}: MovieProps){
     return (
-        <>
-            <Header />
-            <div className="w-screen overflow-hidden max-md:overflow-auto h-screen">
-                <div className={`h-screen mt-[88px]`}>
-                    <div className="h-full w-full bg-transparent">
-                        <div className="flex h-full w-full justify-center items-center">
-                            <main className="flex items-center gap-8 max-w-[850px] max-h-[330px] max-md:max-h-full max-md:translate-y-0 max-md:flex-col max-md:py-4 -translate-y-14">
-                                <Image
-                                height={450}
-                                width={300}
-                                alt={'Poster da Série Stranger Things'}
-                                src={PosterURL ? PosterURL : posterDefault}
-                                className=' w-[300px]'
-                                /> 
-                                <div className="infos flex flex-col gap-2 max-md:px-4">
-                                    <div className="flex justify-center">
-                                        <h1 className="text-white-primary text-lg">{datas.title}</h1>
-                                        <h1 className="text-white-primary text-lg">({datas.release_date})</h1>
-                                    </div>
-                                    <div className="description">
-                                        <p className="text-white-primary text-md text-justify max-md:text-sm">
-                                            <span className="max-md:text-sm">Visão Geral: </span>
-                                            {datas.overview}
-                                        </p>
-                                    </div>
-                                </div>
-                            </main>
-                        </div>
-                        
-                    </div>
-
-                </div>
-            </div>
-        </>
+    <div className="w-screen overflow-hidden max-md:overflow-auto h-screen">
+             <div className={`h-screen mt-[88px]`}>
+                 <div className="h-full w-full bg-transparent">
+                     <div className="flex h-full w-full justify-center items-center">
+                         <main className="flex items-center gap-8 max-w-[850px] max-h-[330px] max-md:max-h-full max-md:translate-y-0 max-md:flex-col max-md:py-4 -translate-y-14">
+                             <Image
+                             height={450}
+                             width={300}
+                             alt={'Poster da Série Stranger Things'}
+                             src={PosterPath}
+                             className=' w-[300px]'
+                             /> 
+                             <div className="infos flex flex-col gap-2 max-md:px-4">
+                                 <div className="flex justify-center">
+                                     <h1 className="text-white-primary text-lg">
+                                        {title}
+                                    </h1>
+                                     <h1 className="text-white-primary text-lg">({Release_date})</h1>
+                                 </div>
+                                 <div className="description">
+                                     <p className="text-white-primary text-md text-justify max-md:text-sm">
+                                         <span className="max-md:text-sm">Visão Geral: </span>
+                                         {Overview}
+                                     </p>
+                                 </div>
+                             </div>
+                             
+                         </main>
+                     </div>  
+                 </div>
+             </div>
+         </div>
     )
 }

9ca9b599a8fb4fc135c1b0774b75b434ac279294
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-01-25 18:23:22 -0300

    Fix: test next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,39 +1,65 @@
+import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { v4 as uuidv4 } from 'uuid';
 
-export default NextAuth({
+const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    }, 
     providers: [
         CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "E-mail", type: "text", placeholder: "Digite seu Email" },
-                password: { label: "Password", type: "password", placeholder: "Digite sua senha" }
-            },
-            async authorize(credentials: any) {
-                const user = { id: uuidv4(), credentials:credentials }
-                 if (user) {
-                    return user
-                } else {
+            type: 'credentials',
+            credentials: {},
+            authorize(credentials, req){
+                const {email, password} = credentials as {
+                    email: string,
+                    password: string
+                }
+
+                if(email !== 'test2@gmail.com' && password !== '12345678'){
                     return null
+                }else return {
+                    id: uuidv4(),
+                    email: email
                 }
             }
         })
-    ],
-    callbacks: {
-        jwt: ({ token, user }) => {
-            if (user) { token.id = user.id }
-            return token;
-        },
-        session: ({ session, token }: any) => {
-            if (token)  {session.id = token.id};
-            return session;
-        },
-    },
-    secret: 'd5dccca540c42789ea914b2d41b6f08',
-    jwt: { },
-    pages: {
-        signIn: '/Entrar/',
-        newUser: '/Registrar/'
-    }
-})
+    ]
+}
+export default NextAuth(authOptions)
+// export default NextAuth({
+//     providers: [
+//         CredentialsProvider({
+//             name: "Credentials",
+//             credentials: {
+//                 email: { label: "E-mail", type: "text", placeholder: "Digite seu Email" },
+//                 password: { label: "Password", type: "password", placeholder: "Digite sua senha" }
+//             },
+//             async authorize(credentials: any) {
+//                 const user = { id: uuidv4(), credentials:credentials }
+//                  if (user) {
+//                     return user
+//                 } else {
+//                     return null
+//                 }
+//             }
+//         })
+//     ],
+//     callbacks: {
+//         jwt: ({ token, user }) => {
+//             if (user) { token.id = user.id }
+//             return token;
+//         },
+//         session: ({ session, token }: any) => {
+//             if (token)  {session.id = token.id};
+//             return session;
+//         },
+//     },
+//     secret: 'd5dccca540c42789ea914b2d41b6f08',
+//     jwt: { },
+//     pages: {
+//         signIn: '/Entrar/',
+//         newUser: '/Registrar/'
+//     }
+// })

d754383292875b1eb540cc5a95445412071e2614
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-01-25 21:55:56 -0300

    Fix: remove item from API

--- api/routes/list/remove.js
+++ api/routes/list/remove.js
@@ -18,12 +18,10 @@ router.patch('/list/remove/', async (req, res) => {
     try {
         const list = user.titles
 
-    	list.map(item =>{
-            const index = item.name.indexOf(name);
-            
-            if(index !== -1 ){
-                if(index === 0){list.splice(index, 1)}
-                else list.splice(index+1, 1)
+    	list.map((item, index) =>{
+            if(name === item.name){
+                if(index === 0){list.splice(index)}
+                else list.splice(index)
             }
         })
         await User.findOneAndUpdate(

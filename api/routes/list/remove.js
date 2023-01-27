5122676183c9aafcc4f8bd7f61e8e302c2523c87
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-01-27 06:35:25 -0300

    Fix: Remove title from API

--- api/routes/list/remove.js
+++ api/routes/list/remove.js
@@ -16,20 +16,14 @@ router.patch('/list/remove/', async (req, res) => {
     if (!user) return res.status(422).json({ msg: 'Usuário não encontrado!' })
 
     try {
-        const titles = user.titles
-
-    	titles.map((item, index) =>{
-            if(name === item.name){
-                titles.splice(index, 1)
-            }
-        })
-        
+        const Alltitles = user.titles
+        const newTitles = Alltitles.filter(title => title.name != name);
+        user.titles = newTitles
         await user.save()
-        res.status(200).json({ titles })
-    
+        res.status(200).json({ user })
 
-    } catch (error) {
-        res.status(500).json({ msg: error })
+    } catch (err) {
+        res.status(500).json({ msg: err })
     }
 })
 
